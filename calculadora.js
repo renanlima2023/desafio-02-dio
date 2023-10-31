7171da7d30ed06d15f52afaa343c499a2c48e336
Renanlima2023 <renan.lima04@gmail.com>
2023-10-31 10:49:45 -0300

    one more commit

--- calculadora.js
+++ calculadora.js
@@ -1,23 +1,27 @@
-function calcularNivel(vitorias, derrotas) {
-    const saldoVitorias = vitorias - derrotas;
-    let nivel = "";
-  
-    if (vitorias < 10) {
-      nivel = "Ferro";
-    } else if (vitorias >= 11 && vitorias <= 20) {
-      nivel = "Bronze";
-    } else if (vitorias >= 21 && vitorias <= 50) {
-      nivel = "Prata";
-    } else if (vitorias >= 51 && vitorias <= 80) {
-      nivel = "Ouro";
-    } else if (vitorias >= 81 && vitorias <= 90) {
-      nivel = "Diamante";
-    } else if (vitorias >= 91 && vitorias <= 100) {
-      nivel = "Lendário";
-    } else {
-      nivel = "Imortal";
-    }
-  
-    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`);
+const nivel = ["Ferro", "Bronze","Ouro","Diamante", "Lendário", "Imortal"];
+let saldoVitorias = calcularNivelCompleto(3000, 1999)
+
+function calcularNivelCompleto(vitorias, derrotas) {
+  let saldoVitorias = vitorias - derrotas;
+
+  let nivel = "";
+  if (saldoVitorias < 10) {
+    nivel = "Ferro";
+  } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+    nivel = "Bronze";
+  } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+    nivel = "Prata";
+  } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+    nivel = "Ouro";
+  } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+    nivel = "Diamante";
+  } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
   }
-  
+
+  console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`);
+}
+
+calcularNivelCompleto(15, 5); // Example usage
